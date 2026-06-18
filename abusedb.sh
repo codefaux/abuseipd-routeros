572fdb1afa10f10da2b46f53e77b001ebcb337d6
codefaux <codefaux@solacenet.org>
2026-06-18 16:11:26 -0700

    Rename/reorder vars, correct upstream's abuseidb typo

--- abusedb.sh
+++ abusedb.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PLIK=/tmp/abuseidb.txt
-MT_PLIK=/tmp/abuseidb.rsc
-MT_SSH_ID=/path/to/ssh/id_rsa
 SOURCE_URL="https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/refs/heads/main/abuseipdb-s100-14d.ipv4"
+UNPROCESSED_LIST=/tmp/abuseipdb.txt
+OUTPUT_LIST=/tmp/abuseipdb.rsc
+ROUTEROS_SSH_KEYFILE=/path/to/ssh/id_rsa
 # https://github.com/borestad/blocklist-abuseipdb - [1d,3d,7d,14d,30d,60d,90d,120d,180d,365d,all]
 
-rm -rf $PLIK
-rm -rf $MT_PLIK
+rm -rf $UNPROCESSED_LIST
+rm -rf $OUTPUT_LIST
 
-curl -fJLs $SOURCE_URL -o $PLIK
+curl -fJLs $SOURCE_URL -o $UNPROCESSED_LIST
 
-echo "/ip firewall address-list" >> $MT_PLIK
-echo "/ip firewall address-list remove [/ip firewall address-list find list=abuseidb]" >> $MT_PLIK
+echo "/ip firewall address-list" > $OUTPUT_LIST
+echo "/ip firewall address-list remove [/ip firewall address-list find list=abuseipdb]" >> $OUTPUT_LIST
 
 while IFS= read -r ip
 do
@@ -22,8 +22,8 @@ do
   ip="${ip%"${ip##*[![:space:]]}"}"
 
   [[ -z $ip ]] && continue
-echo "add list=abuseidb address=$ip timeout=2d" >> $MT_PLIK
+echo "add list=abuseipdb address=$ip timeout=2d" >> $OUTPUT_LIST
 done < "$PLIK"
 
-scp -i $MT_SSH_ID -P $MT_PORT $MT_PLIK $MT_LOGIN@$MT_HOST:/abuseidb.rsc
-ssh -i $MT_SSH_ID -o StrictHostKeyChecking=no -p $MT_PORT -l $MT_LOGIN $MT_HOST /import abuseidb.rsc
+scp -i $ROUTEROS_SSH_KEYFILE -P $MT_PORT $OUTPUT_LIST $MT_LOGIN@$MT_HOST:/abuseipdb.rsc
+ssh -i $ROUTEROS_SSH_KEYFILE -o StrictHostKeyChecking=no -p $MT_PORT -l $MT_LOGIN $MT_HOST /import abuseidb.rsc
